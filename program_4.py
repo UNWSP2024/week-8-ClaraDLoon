a2e080763cd7fbbb78e26f20f2760c25ee156cf3
ClaraDLoon <ClaraDLoon@outlook.com>
2024-10-24 22:24:21 -0500

    Create program_4.py

---
+++ program_4.py
@@ -0,0 +1,15 @@
+#Program #4: Course Info
+#Clara Riley
+#Luke Snell
+#10/24/24
+
+#BEGIN
+  #WHILE user wants to add courses
+    #Prompt user for course ID
+    #Prompt user for course name
+    #Add course ID and name to courses dictionary
+  #Prompt user for subject
+  #FOR each course in courses
+    #IF course ID starts with subject
+      #Display course ID and course name
+#END
